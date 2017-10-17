8d733df1e84a05d1d3ff535351e4b070958f3c44
rashidmuhammad07 <mrashid@ucdavis.edu>
2017-10-16 18:20:01 -0700

    Add a jumping dancer

---
+++ src/jumpingDancer.js
@@ -0,0 +1,25 @@
+var makeJumpingDancer = function(top, left, timeBetweenSteps) {
+  makeDancer.call(this, top, left, timeBetweenSteps / 4);
+  this.top = top;
+};
+
+makeJumpingDancer.prototype = Object.create(makeDancer.prototype);
+makeJumpingDancer.prototype.construtor = makeJumpingDancer;
+
+  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
+  // so we must keep a copy of the old version of this function
+makeJumpingDancer.prototype.oldStep = makeDancer.prototype.step;
+
+makeJumpingDancer.prototype.step = function() {
+  // call the old version of step at the beginning of any call to this new version of step
+  this.oldStep();
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for this and
+  // other effects you can use on a jQuery-wrapped html tag.
+console.log(Math.floor(this.top), parseInt(this.$node.css('top')));
+  if (Math.floor(this.top) === parseInt(this.$node.css('top'))) {
+    this.$node.css({top: '-=10px'});
+  } else {
+    this.$node.css({top: this.top});
+  }
+};
