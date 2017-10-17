c7a0552a7c312dd71d554d95f7598b96b80df97f
rashidmuhammad07 <mrashid@ucdavis.edu>
2017-10-16 18:47:04 -0700

    Remove console.log this.oldStep

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -8,18 +8,9 @@ makeBlinkyDancer.prototype.construtor = makeBlinkyDancer;
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
 makeBlinkyDancer.prototype.oldStep = makeDancer.prototype.step;
-// makeBlinkyDancer.prototype.oldStep = function() {
-//   // the basic dancer doesn't do anything interesting at all on each step,
-//   // it just schedules the next step
-//   console.log('step');
-// };
-// makeBlinkyDancer.prototype.oldStep = function() {
-//   return this.step;
-// };
 
 makeBlinkyDancer.prototype.step = function() {
   // call the old version of step at the beginning of any call to this new version of step
-console.log(this.oldStep);
   this.oldStep();
   // toggle() is a jQuery method to show/hide the <span> tag.
   // See http://api.jquery.com/category/effects/ for this and
